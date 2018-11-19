0690da62331b626a642a42e2111c96c5a218cea7
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-19 22:26:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+configure do
+	enable :sessions
+	set :session_secret, "67739624a7153b317530f398aa6ebb95e57450466854e62819c62ba1562b4356d68176ffdce5c32ba246f3b8cc459190832435897fe0f99a8dbb72324fbf03ea"
+end
+
 end
